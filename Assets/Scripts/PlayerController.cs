fc62c929dcd41f757478538e5f4026ee45ff1ac0
Patrick Sheng <sheng.patrick@gmail.com>
2026-04-30 20:38:52 +1200

    feat: created player object, added input action map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  [SerializeField] private float moveSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private PlayerInputActions _inputActions;
+  private Rigidbody2D _rb;
+  private Vector2 _moveInput;
+
+  private void Awake()
+  {
+    _rb = GetComponent<Rigidbody2D>();
+    _inputActions = new PlayerInputActions();
+  }
+
+  private void OnEnable()
+  {
+    _inputActions.Player.Enable();
+  }
+
+  private void OnDisable()
+  {
+    _inputActions.Player.Disable();
+  }
+
+  private void Update()
+  {
+    _moveInput = _inputActions.Player.Move.ReadValue<Vector2>();
+  }
+
+  private void FixedUpdate()
+  {
+    _rb.linearVelocity = _moveInput * moveSpeed;
+  }
 }
