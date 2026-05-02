d0ce5bcf705648a74e9ab00466bfa179803d4692
Patrick Sheng <sheng.patrick@gmail.com>
2026-05-02 15:51:49 +1200

    feat: player can now 'collect' the fish on collision

--- Assets/Scripts/FishInteraction.cs
+++ Assets/Scripts/FishInteraction.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class FishInteraction : MonoBehaviour
 {
-  private void OnCollisionEnter2D(Collision2D collision)
+  private void OnTriggerEnter2D(Collider2D collision)
   {
     if (collision.gameObject.CompareTag("Player"))
     {
       collision.gameObject.GetComponent<PlayerController>().CollectFish();
       Debug.Log("Fish collected");
+      Destroy(gameObject);
     }
   }
 }
